7a62676c7c3381924742ef3c3b1053e4d5efe941
sylviawangfr <32857916+sylviawangfr@users.noreply.github.com>
2018-03-12 13:08:54 +0800

    add format junit

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha', 'html'],
+    reporters: ['mocha', 'html', 'junit'],
 
     htmlReporter: {
       outputDir: 'karma', // where to put the reports
